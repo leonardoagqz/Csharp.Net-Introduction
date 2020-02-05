d7c0c3d99bb049ae32fbf9656cd3ab652c443393
Leonardo <leonardoagqz@gmail.com>
2020-02-05 20:53:52 -0200

    30 Funcoes(sintax)

---
+++ Funções (sintax)/Funcoes(sintax)/Funcoes(sintax)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Funcoes_sintax_ {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Descobrir o maior de três números");
+            Console.WriteLine("");
+            Console.WriteLine("Digite três números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            //chamando a funcao
+            int resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("Maior = " + resultado);
+
+        }
+
+        //criando a funcao
+        //static = para poder funcionar dentro da funcao principal ^
+        //int = o tipo da saida da funcao
+        //Maior = nome da funcao
+        //() = parametros, dados de entrada da funcao
+        static int Maior(int x, int y, int z) {
+            int maior;
+            if (x > y && x > z) {
+                maior = x;
+            }
+            else if (y > z) {
+                maior = y;
+            }
+            else {
+                maior = z;
+            }
+            return maior;
+
+
+        }
+    }
+
+
+}
+
