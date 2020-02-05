86c23421207bce03a710d9955cfc0fce0e0af9a1
Leonardo <leonardoagqz@gmail.com>
2020-02-05 21:54:10 -0200

    31 Debugging Visual Studio

---
+++ Debugging Visual Studio/Debugging Visual Studio/Debugging Visual Studio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Debugging_Visual_Studio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Debugging");
+            Console.WriteLine("");
+
+            /*
+             F9 para marcar o BreakPoint ao selecionar a linha do código.
+             F5 para iniciar o debug.
+             F10 para executar passo a passo.
+             F11 para executar passo a passo dentro de funções.
+             SHIFT F5 para interromper o debug
+
+            Obs: executar com F10, o mesmo não entra dentro das funções
+
+              */
+
+
+
+        }
+
+    }
+}
+
