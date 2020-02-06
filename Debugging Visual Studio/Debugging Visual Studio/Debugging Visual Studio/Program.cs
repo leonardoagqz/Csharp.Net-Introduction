f6179e7361896448eeaf7c9561a4f8e8f9333cf3
Leonardo <leonardoagqz@gmail.com>
2020-02-05 22:04:25 -0200

    31 Debugging Visual Studio v2

--- Debugging Visual Studio/Debugging Visual Studio/Debugging Visual Studio/Program.cs
+++ Debugging Visual Studio/Debugging Visual Studio/Debugging Visual Studio/Program.cs
@@ -10,7 +10,7 @@ namespace Debugging_Visual_Studio {
              F9 para marcar o BreakPoint ao selecionar a linha do código.
              F5 para iniciar o debug.
              F10 para executar passo a passo.
-             F11 para executar passo a passo dentro de funções.
+             F11 entra dentro de funções para executar passo a passo com F10
              SHIFT F5 para interromper o debug
 
             Obs: executar com F10, o mesmo não entra dentro das funções
