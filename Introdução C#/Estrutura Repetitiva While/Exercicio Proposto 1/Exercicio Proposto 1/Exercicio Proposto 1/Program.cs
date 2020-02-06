3795491ddf74abd18a8f834461a74586cc004270
Leonardo <leonardoagqz@gmail.com>
2020-02-06 21:59:00 -0200

    Exercicio proposto 1

---
+++ Introdução C#/Estrutura Repetitiva While/Exercicio Proposto 1/Exercicio Proposto 1/Exercicio Proposto 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio_Proposto_1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" *** Exercício Proposto 1 ***");
+            Console.WriteLine("");
+            Console.WriteLine("Escreva um programa que repita a leitura de uma senha até que ela seja" +
+                              "válida. Para cada leitura de senha incorreta informada, escrever a " +
+                              "mensagem ''Senha Invalida''.Quando a senha for informada corretamente " +
+                              "deve ser impressa a mensagem ''Acesso Permitido'' e o algoritmo encerrado." +
+                              "Considere que a senha correta é o valor 2002.");
+            Console.WriteLine("");
+            Console.WriteLine("Informe a senha");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                
+                Console.WriteLine("Senha Incorreta!");
+                senha = int.Parse(Console.ReadLine());             
+            }
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
