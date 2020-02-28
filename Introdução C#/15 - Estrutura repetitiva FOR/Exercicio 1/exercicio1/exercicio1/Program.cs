33a0a8e72c70ac45f1d2bad9e3ff1478151bd2b8
Leonardo <leonardoagqz@gmail.com>
2020-02-28 19:09:31 -0300

    exercicio1 FOR

---
+++ Introdução C#/15 - Estrutura repetitiva FOR/Exercicio 1/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X," +
+                " um valor por linha, inclusive o X, se for o caso.");
+            Console.WriteLine("");
+            Console.Write("Informe o valor: ");
+            double x = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
