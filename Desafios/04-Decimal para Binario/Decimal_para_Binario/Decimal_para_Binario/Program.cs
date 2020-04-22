343433f473cf84cc3fb6b1a81aa7506dd977f180
Leonardo <leonardoagqz@gmail.com>
2020-04-22 16:54:44 -0300

    Desafio 04 Decimal para Binario

---
+++ Desafios/04-Decimal para Binario/Decimal_para_Binario/Decimal_para_Binario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Decimal_para_Binario {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("Infome valor Decimal!");
+            while (true) {
+
+                int xvalorentrada = int.Parse(Console.ReadLine());
+
+                int xdecimal = xvalorentrada;
+                string xbinario = "";
+
+                while (xvalorentrada > 0) {
+
+                    int xresto = xvalorentrada % 2;
+                    if (xresto == 0) {
+                        xbinario = "0" + xbinario;
+                    }
+                    else {
+                        xbinario = "1" + xbinario;
+
+                    }
+                    xvalorentrada = xvalorentrada / 2;
+                    
+                }
+                Console.WriteLine(xdecimal + " = " + xbinario);
+            }   
+         
+        }
+     }
+ }
