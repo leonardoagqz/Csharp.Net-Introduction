a2d51978498c91ea6d4f9e016ad973a75ef0f1ac
Leonardo <leonardoagqz@gmail.com>
2020-04-25 18:09:34 -0300

    aula 38 resolvendo problema sem orientação a objetos

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/38 resolvendo um problema sem orientacao a objetos/SemOriObj/SemOriObj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+namespace SemOriObj {
+    class Program {
+        static void Main(string[] args) {
+            //criando variáveis do triângulo x e y
+
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double xp = (xA + xB + xC) / 2;
+            double areaX = Math.Sqrt(xp * (xp - xA) * (xp - xB) * (xp - xC));
+            
+            double yp = (yA + yB + yC) / 2;
+            double areaY = Math.Sqrt(yp * (yp - yA) * (yp - yB) * (yp - yC));
+
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            if (areaX > areaY) {
+                Console.WriteLine("Área Maior: X");
+            }
+            else { Console.WriteLine("Área Maior: Y"); }
+
+        }
+    }
+}
