e12b03a1f9cb42159c98b148c1e34b0cedab22d6
Leonardo <leonardoagqz@gmail.com>
2020-04-25 19:10:52 -0300

    39 resolvendo um problema com orientacao a objetos

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/39 criando classe com 3 atributos para representar um triangulo/39Criando/39Criando/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+namespace _39Criando { /*Criando uma classe com três atributos para representar melhor o triângulo.*/
+    class Program {
+        static void Main(string[] args) {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double xp = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(xp * (xp - x.A) * (xp - x.B) * (xp - x.C));
+
+            double yp = (y.A + y.B + y.C) / 2;
+            double areaY = Math.Sqrt(yp * (yp - y.A) * (yp - y.B) * (yp - y.C));
+
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            if (areaX > areaY) {
+                Console.WriteLine("Área Maior: X");
+            }
+            else { Console.WriteLine("Área Maior: Y"); }
+
+        }
+    }
+}
---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/39 criando classe com 3 atributos para representar um triangulo/39Criando/39Criando/Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _39Criando {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+
+    }
+}
