aa00ede8a753d781f7d881a9beefe92c12eb3729
Leonardo <leonardoagqz@gmail.com>
2020-05-08 18:45:53 -0300

    Criado Classe e Método Triângulo para remover do código principal os cálculos

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/41 Criando um método para obtermos os benefícios de reaprov/Aula41/Aula41/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System; //Importação de Dependências
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula41 {
+
+     //Nome da Classe: Triangulo
+    class Triangulo {
+
+        //Atributos (Lados do Triangulo) A,B,C
+        public double A;
+        public double B;
+        public double C;
+
+        //Método Calcular a Area
+        //Função Area
+        public double Area() {
+            double p = (A + B + C) / 2;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+
+            //OU
+
+         /*
+        public double Area() {
+            double p = (A + B + C) / 2;
+            return raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+         */
+
+        }
+
+    }
+}
+
+/*
+ Benefícios de se calcular a área de um triângulo por meio de MÉTODO dentro da CLASSE Triangulo:
+
+    1)Reaproveitamento de código: eleiminamos o código repetido no programa principal.
+
+    2)Delegação de responsabilidades: quem deve ser responsável por saber como calcular a área de um triângulo é o proprio triângulo.
+      A lógica do cálculo da área não deve estar em outro lugar.
+     
+*/
---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/41 Criando um método para obtermos os benefícios de reaprov/Aula41/Aula41/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+namespace Aula41 {
+    class Program {
+        static void Main(string[] args) {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            if (areaX > areaY) {
+                Console.WriteLine("Área Maior: X");
+            }
+            else { Console.WriteLine("Área Maior: Y"); }
+
+
+        }
+    
+    
+    }
+
+
+}
