a4781b5448c8380fd715d78a4e82007b0ea2562a
Leonardo <leonardoagqz@gmail.com>
2020-05-09 11:33:45 -0300

    Aulas 42 e 43 Resolvendo problemas e (Object e ToString)

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/42 43 Resolvendo problemas e (Object e ToString)/Aula42/Aula42/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula42 {
+    class Produto {
+
+        // ATRIBUTOS
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //MÉTODOS
+        public double ValorTotalEmEstoque() {
+
+            return Preco * Quantidade;
+        
+        }
+
+        //Sobreposição
+        public override string ToString() {
+            return Nome 
+                +", $ "+string.Format("{0:n2}",Preco)
+                +", "
+                +" unidades, Total: $ " 
+                +string.Format("{0:n2}",ValorTotalEmEstoque());
+        }
+
+    }
+}
---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/42 43 Resolvendo problemas e (Object e ToString)/Aula42/Aula42/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace Aula42 {
+    class Program {
+
+        /*Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). Em seguida:
+         
+            - Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque)
+            - Realizar uma entrada no estoque e mostrar novamente os dados do produto 
+            - Realizar uma saída no estoque e mostrar novamente os dados do produto 
+            
+             
+             Para resolver este problema, você deve criar uma CLASSE conforme abaixo:
+             
+             Produto
+               ATRIBUTOS
+             -Nome:string
+             -Preco: double
+             -Quantidade:int
+             ---------------
+               MÉTODOS
+             +ValorTotalEmEstoque():double
+             +AdicionarProdutos(quanty:int):void
+             +RemoveerProdutos(quanty:int):void
+             
+             
+             */
+
+        static void Main(string[] args) {
+
+            Produto p = new Produto(); // Criando variável e instanciando 
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Dados do Produto: "+ p);
+                
+        }
+    }
+}
