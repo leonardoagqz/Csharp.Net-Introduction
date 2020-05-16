6f6235a61c3fa7474fdf4d5db203018e5cd0046a
Leonardo <leonardoagqz@gmail.com>
2020-05-16 20:15:20 -0300

    Aula 45 Exerc 3

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/45 Exerc (Classes, Atributos, Métodos)/Exerc 3/Exerc_3/Exerc_3/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_3 {
+    class Aluno {
+
+        public string Nome;
+        public double nota1,nota2,nota3;
+
+        public double NotaFinal() {
+            return nota1 + nota2 + nota3;
+        }
+
+        public bool Aprovado() {
+            if (NotaFinal() >= 60.0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public double NotasRestantes() {
+            if (Aprovado()) {
+                return 0.0;
+            }
+            else {
+                return 60.0 - NotaFinal();
+            }
+        }
+
+        public override string ToString() {
+
+            return ("NOTA FINAL: " + NotaFinal());
+
+            
+
+        }
+
+       
+
+
+
+    }
+}
---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/45 Exerc (Classes, Atributos, Métodos)/Exerc 3/Exerc_3/Exerc_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_3 {
+
+    /*Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três
+     trimestres do ano (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada)
+     Ao final , mostrar qual a nota final do aluno no ano. Dizer também se o aluno está 
+     APROVADO ou  REPROVADO e, em caso negativo, quantos pontos faltam para o aluno obter
+     o mínimo para ser aprovado (que é 60 pontos).*/
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            Console.Write("Nota 1: ");
+            aluno.nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 2: ");
+            aluno.nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 3: ");
+            aluno.nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(aluno);
+
+            if (aluno.Aprovado()) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTAM " + aluno.NotasRestantes() + " PONTOS PARA MÉDIA 60.0");
+            }
+        }
+    }
+}
