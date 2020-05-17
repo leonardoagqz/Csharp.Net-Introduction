968ec72265d9178abbfb21f541958e72966dedcc
Leonardo <leonardoagqz@gmail.com>
2020-05-17 20:07:43 -0300

    Aula 48 exercicio membros estaticos

---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/48 Exerc Membros Estáticos/Exerc_Membros_Estaticos/Exerc_Membros_Estaticos/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Membros_Estaticos {
+    class ConversorDeMoeda {
+
+        public static double Conversor(double c, double d) {
+            return c * d;
+        
+        
+        }
+
+    }
+}
---
+++ 03 Classes, Atributos, Métodos, Membros Estáticos/48 Exerc Membros Estáticos/Exerc_Membros_Estaticos/Exerc_Membros_Estaticos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_Membros_Estaticos {
+
+    /*Faça um programa para ler a cotção do dólar, e depois um valor em dólares a ser comprado por uma pessoa em rais. 
+     Informa quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar.
+    
+    Exemplo:
+    Qual é a cotção do dólar?: 3.10
+    Quantos dólares voçê vai comprar?: 200.00
+    Valor a ser pago em reais = 620,00
+    
+    */
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dólar?: ");
+            double CotDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantos dólares voçê vai comprar?: ");
+            double ValDolComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.Conversor(CotDolar, ValDolComprar);
+
+            Console.WriteLine("Valor a ser pago em reais = "+ string.Format("{0:n2}", resultado) );
+        }
+    }
+}
