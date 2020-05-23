978ede002888c734ae85c4be9b91d0321a690d53
Leonardo <leonardoagqz@gmail.com>
2020-05-23 15:02:58 -0300

    Aula 51 Construtores

---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/51 Construtores/Construtores.Intro/Construtores.Intro/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula42 {
+    class Produto {
+
+        // ATRIBUTOS
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //CONSTRUTOR
+        //Obriga informar um nome, preço e quantidade.
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        
+        }
+
+        //MÉTODOS
+        public double ValorTotalEmEstoque() {
+
+            return Preco * Quantidade;
+
+        }
+
+        //void=vazio, a funão não irá retornar nenhum valor
+        public void AdicionarProdutos(int quantidade) {
+
+            Quantidade = Quantidade + quantidade;
+            //OU
+            // Quantidade += quantidade;
+
+
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+
+
+        //Sobreposição
+        public override string ToString() {
+            return Nome
+                + ", $ " + string.Format("{0:n2}", Preco)
+                + ", "
+                + Quantidade + " unidades, Total: $ "
+                + string.Format("{0:n2}", ValorTotalEmEstoque());
+        }
+
+    }
+}
---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/51 Construtores/Construtores.Intro/Construtores.Intro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace Aula42 {
+    class Program {
+
+        /*Código da aula 44 reaproveitado para treinamento de Construtores*/
+
+
+
+        static void Main(string[] args) {
+                  
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Usando o construtor para informar os valores nome, preço, quantidade
+            Produto produto1 = new Produto(nome,preco,quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + produto1);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto1.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto1);
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque:");
+            qte = int.Parse(Console.ReadLine());
+            produto1.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto1);
+
+
+        }
+    }
+}
