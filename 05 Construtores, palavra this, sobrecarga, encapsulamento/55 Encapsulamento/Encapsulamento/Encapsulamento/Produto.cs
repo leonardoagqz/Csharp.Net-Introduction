a38bbfe34f975bf3542504d21fcc94e0f8c9e3a8
Leonardo <leonardoagqz@gmail.com>
2020-05-30 09:07:32 -0300

    Encapsulamento

---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/55 Encapsulamento/Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento {
+    class Produto {
+
+        // ATRIBUTOS
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        //CONSTRUTOR PADRAO
+        public Produto() { 
+        }
+
+        //CONSTRUTOR
+        //Obriga informar um nome, preço e quantidade.
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+
+        }
+
+        //Implementando Método Get e Set
+
+        //Implementando Método Get para obter o nome do produto
+        public string GetNome() {
+            return _nome;
+        }
+
+        //Implementando Método Set para alterar o nome do produto
+        public void SetNome(string nome) {
+
+  //se o nome for diferente de vazio e a quantidade de letras for maior que 1
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+             
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public int GetQuantidade() { 
+        return _quantidade;
+        }
+
+        //MÉTODOS
+        public double ValorTotalEmEstoque() {
+
+            return _preco * _quantidade;
+
+        }
+
+        //void=vazio, a funão não irá retornar nenhum valor
+        public void AdicionarProdutos(int quantidade) {
+
+            _quantidade = _quantidade + quantidade;
+            //OU
+            // Quantidade += quantidade;
+
+
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            _quantidade -= quantidade;
+        }
+
+
+
+        //Sobreposição
+        public override string ToString() {
+            return _nome
+                + ", $ " + string.Format("{0:n2}", _preco)
+                + ", "
+                + _quantidade + " unidades, Total: $ "
+                + string.Format("{0:n2}", ValorTotalEmEstoque());
+        }
+
+    }
+}
---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/55 Encapsulamento/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace Encapsulamento {
+    class Program {
+
+       
+        static void Main(string[] args) {
+
+
+            Produto prod = new Produto("TV",500,10);
+
+            prod.SetNome("TV 4k");
+
+            Console.WriteLine(prod.GetNome());
+            Console.WriteLine(prod.GetPreco());
+            Console.WriteLine(prod.GetQuantidade());
+
+        }
+    }
+}
