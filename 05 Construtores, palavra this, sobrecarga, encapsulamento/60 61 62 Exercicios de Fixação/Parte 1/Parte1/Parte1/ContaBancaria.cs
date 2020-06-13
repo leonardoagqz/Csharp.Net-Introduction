ff900bc22c62e27ae22e987d90311ccbdea2c474
Leonardo <leonardoagqz@gmail.com>
2020-06-13 20:05:41 -0300

    Exercicios utilizando Classe ContaBancaria Construtores de 2 e 3 arqgumentos, Métodos Deposito e Saque aulas 61 62

---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/60 61 62 Exercicios de Fixação/Parte 1/Parte1/Parte1/ContaBancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parte1 {
+    class ContaBancaria {
+        //Atributos
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+
+        public Double Saldo { get; private set; }
+
+
+        //Construtores
+
+        //Contrutor com dois argumentos
+        public ContaBancaria(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+           
+        }
+
+        //Contrutor com três argumentos e utilizando o "this" para receber o "numero" e "titular"
+        public ContaBancaria(int numero, string titular, double depositoinicial) :this(numero,titular) {
+            
+            Deposito(depositoinicial);
+                
+        }
+
+        //Métodos
+
+        //será void pq não terá retorno, apenas irá no Saldo e alteraro valor do mesmo. 
+        public void Deposito(double quantia) {
+
+            Saldo = Saldo + quantia; 
+            //ou Saldo += quantida;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia; //desconta a quantia
+            Saldo -= 5.0; //desconta a taxa
+        }
+
+
+        //Texto que exibe os dados no final
+        public override string ToString() {
+            return "Conta: " + Numero 
+                + " , Titular: " 
+                + Titular + ", Saldo: $ " 
+                + string.Format("{0:n2}",Saldo);
+        }
+
+
+
+
+    }
+}
---
+++ 05 Construtores, palavra this, sobrecarga, encapsulamento/60 61 62 Exercicios de Fixação/Parte 1/Parte1/Parte1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Parte1 {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria  conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoinicial = double.Parse(Console.ReadLine());
+
+                //instanciar a Conta com os dados recebidos
+                //Quando for utilizar os 3 argumentos (numero da conta, nome do titular e deposito inicial)
+                conta = new ContaBancaria(numero,titular,depositoinicial);
+            }
+
+            //instanciar a Conta com os dados recebidos
+            //Quando for utilizar somente 2 argumentos (numero da conta e nome do titular)
+            else { conta = new ContaBancaria(numero, titular); }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta: " + conta);
+
+            //Depositando
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+
+            //chamar no objeto conta a operação deposito
+            conta.Deposito(deposito);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta: " + conta);
+
+            //Sacando
+            Console.WriteLine();
+            Console.Write("Entre um valor para Saque: ");
+            double saque = double.Parse(Console.ReadLine());
+
+            //chamar no objeto conta a operação saque
+            conta.Saque(saque);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta: " + conta);
+
+
+
+
+
+        }
+
+        
+
+    }
+}
