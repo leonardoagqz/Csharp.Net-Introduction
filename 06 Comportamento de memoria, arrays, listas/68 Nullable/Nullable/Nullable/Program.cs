2479d5f07d4079ff37b70c35151595f4099d6818
Leonardo <leonardoagqz@gmail.com>
2020-06-14 18:52:56 -0300

    68 Nullable, double? x = null;, Metódos: GetvalueOrDefault,HasValue, Value

---
+++ 06 Comportamento de memoria, arrays, listas/68 Nullable/Nullable/Nullable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Nullable {
+    class Program {
+        static void Main(string[] args) {
+
+            double? x = null;
+            double? y = 10;
+
+            //não imprime nada
+            Console.WriteLine(x);
+
+            //imprime zero  //pega o valor que esta em x ou o valor default 
+            Console.WriteLine(x.GetValueOrDefault());
+
+            //imprime 10   
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);//imprime False
+            Console.WriteLine(y.HasValue);//imprime True
+
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);//
+            else
+                Console.WriteLine("X é Null");
+            if (y.HasValue)
+                Console.WriteLine(y.Value);//imprime 10
+            else
+                Console.WriteLine("Y é Null");
+
+            Console.WriteLine();
+            //Operador de coalescência nula
+            Console.WriteLine("*** Exemplo de Operador de Coalescência Nula ***");
+
+            double? a = null;
+
+            //se a tiver valor b recebe o valor de a se não recebe 0.0
+            double  b = a ?? 0.0;
+
+
+            Console.WriteLine(a);//imprime nada
+            Console.WriteLine(b);//imprime 0.0, pois a não tem valor
+        }
+    }
+}
