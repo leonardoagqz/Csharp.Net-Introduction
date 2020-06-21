91d09d7f840759fc5a8928913bd14253f4c2d735
Leonardo <leonardoagqz@gmail.com>
2020-06-21 14:08:35 -0300

    70 Vetores 2

---
+++ 06 Comportamento de memoria, arrays, listas/70 Vetores 2/Vetores_2/Vetores_2/Produto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores_2 {
+    class Produto {
+
+        public string  Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ 06 Comportamento de memoria, arrays, listas/70 Vetores 2/Vetores_2/Vetores_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vetores_2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Quantidade de Produtos");
+            int n = int.Parse(Console.ReadLine());
+
+            //criando vetor   //instanciando vetor
+            Produto[] vetor = new Produto[n];
+            
+            //laço para percorrer o vetor
+            for (int i = 0; i < n; i++) {
+
+                //fazendo a leitura do nome e do preço
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine());
+
+                //recebendo os valores das leituras
+                vetor[i] = new Produto { Nome = nome, Preco = preco };
+
+            }
+
+            double soma = 0;
+            for (int i = 0; i < n; i++) {
+
+                soma += vetor[i].Preco;
+                    
+             }
+
+            double media = soma / n;
+            Console.WriteLine("A Média é: "+media);
+                
+        }
+    }
+}
