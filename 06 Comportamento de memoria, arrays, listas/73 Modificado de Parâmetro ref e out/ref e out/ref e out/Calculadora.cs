e0ab2eb0931a532aaec6cd9d91828e9e20fb678a
Leonardo <leonardoagqz@gmail.com>
2020-07-04 17:10:06 -0300

    73 Modificadores de Parâmetro ref e out

---
+++ 06 Comportamento de memoria, arrays, listas/73 Modificado de Parâmetro ref e out/ref e out/ref e out/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ref_e_out {
+    class Calculadora {
+
+        //resolvendo sem ref e out
+        public static int Triple(int x) {
+
+            x = x * 3;
+            return x;
+        }
+
+
+        //utilizando o ref
+        public static void Triple_ref(ref int x) {
+
+            x = x * 3;
+        }
+
+        //utilizando o out
+        public static void Triple_out(int origin, out int result) {
+
+            result = origin * 3;
+        }
+
+    }
+}
---
+++ 06 Comportamento de memoria, arrays, listas/73 Modificado de Parâmetro ref e out/ref e out/ref e out/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ref_e_out {
+    class Program {
+        static void Main(string[] args) {
+
+            //resolvendo sem ref e out 
+            int x = 10;
+            int res = Calculadora.Triple(x);
+            Console.WriteLine(res);
+
+            //utilizando o ref
+            //variavel y é obrigada a ter um valor.
+            int y= 20;
+            Calculadora.Triple_ref(ref y);
+            Console.WriteLine(y);
+
+            //utilizando o out
+            //z recebe o valor e tri retorna o valor triplicado
+            int z = 30;
+            int tri;
+            Calculadora.Triple_out(z, out tri);
+            Console.WriteLine(tri);
+
+        }
+    }
+}
