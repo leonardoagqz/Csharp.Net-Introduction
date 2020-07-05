ece302f71ed9791f34b960492032bb5db6914357
Leonardo <leonardoagqz@gmail.com>
2020-07-05 09:22:21 -0300

    74 Boxing e unboxing

---
+++ 06 Comportamento de memoria, arrays, listas/74 Boxing e unboxing/BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BoxingUnboxing {
+    class Program {
+        static void Main(string[] args) {
+            /*Boxing: é o processo de conversão de um objeto tipo valor para um objeto tipo
+             referência compatível.
+
+             Ex: 
+                 int x = 20; {Na Memoria Stack: x=20}
+                 Object obj = x; {obj será instânciado na Memoria Heap recebendo o valor de x '20'}
+            object é um tipo genérico(referência) da linguagem c#, é compatível com qualquer outro tipo
+
+
+
+            Unboxing: é o processo de conversão de um objeto tipo refência para um objeto tipo valor compátivel
+
+            Ex: basicamente é o processo que desfaz o Boxing
+
+            a variável y esta recendo o valor de obj
+            int y = (int) obj; 
+                
+
+             */
+
+
+
+
+        }
+    }
+}
