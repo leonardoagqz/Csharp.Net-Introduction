56e689754589be33078197dda7377ea05ab280db
Leonardo <leonardoagqz@gmail.com>
2021-01-19 22:12:48 -0200

    Controlando as pecascapturadas

--- ClubdaLuta/Xadrez-main/Xadrez-main/Xadrez-main/Tela.cs
+++ ClubdaLuta/Xadrez-main/Xadrez-main/Xadrez-main/Tela.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using tabuleiro;
 using xadrez;
 
 namespace xadrez_console {
     class Tela {
+
+        public static void ImprimirPartida(PartidaDeXadrez partida) 
+        {
+            ImprimirTabuleiro(partida.Tabuleiro);
+            Console.WriteLine();
+            ImprimirPecasCapturadas(partida);
+            Console.WriteLine("Turno: " + partida.Turno);
+            Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+        }
+
+        private static void ImprimirPecasCapturadas(PartidaDeXadrez partida) 
+        {
+            Console.WriteLine("Peças capturadas:");
+            Console.Write("Brancas: ");
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Branca));
+            Console.WriteLine();
+            Console.Write("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+
+        private static void ImprimirConjunto(HashSet<Peca> conjunto) 
+        {
+            Console.WriteLine("[");
+            foreach (Peca x in conjunto) {
+
+                Console.WriteLine(x + " ");
+            }
+            Console.WriteLine("]");
+        
+        }
         public static void imprimirTabuleiro(Tabuleiro tabuleiro) {
             for (int i = 0; i < tabuleiro.Linhas; i++) {
                 Console.Write(8 - i + " ");
