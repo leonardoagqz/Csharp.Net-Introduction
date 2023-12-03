7401277f70b797820a0ef027ec3e398cc7795ccd
leonardoagqz <leonardoagqz@gmail.com>
2023-12-03 14:38:24 -0300

    Organização Pastas e projetos

---
+++ Site Venda de Lanches .NET MVC/NET_MVC_Vendas/NET_MVC_Vendas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET_MVC_Vendas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
